d7edc91a3531886287dd1472ade4d1871e75ce12
expositomarc <hello@mexposito.com>
2014-10-15 11:58:02 +0200

    Image created

--- Esmeralda/EsmeraldaLibrary.m
+++ Esmeralda/EsmeraldaLibrary.m
@@ -13,7 +13,6 @@
     NSMutableArray *pointsGesture;
     CGPoint *newPointsGesture;
     int *matrixDraw;
-    NSMutableArray *prova; //PROVA
     CGRect rectDraw;
 
 }
@@ -58,9 +57,9 @@
 - (void) startRecognizer {
     
     // We need to frame the image
-    [self getBoundsFromPointsProva];
+    [self getBoundsFromPoints];
     
-    newPointsGesture = (CGPoint *) malloc((sizeof(CGPoint)*[prova count]));
+    newPointsGesture = (CGPoint *) malloc((sizeof(CGPoint)*[pointsGesture count]));
     
     if (newPointsGesture == NULL) {
         
@@ -74,25 +73,16 @@
         // Change points coordinates
         [self changePoints];
         
-        NSLog(@"New points...");
-        
-        for (int i = 0; i < [prova count]; i++) {
-            
-            NSLog(@"%d %d",(int)newPointsGesture[i].x,(int)newPointsGesture[i].y);
-        
-        }
-        
-        NSLog(@"Drawing matrix..");
-        
-        
         // Create matrix
         int error = [self createMatrix];
         
         if (error == 1) {
+            
             NSLog(@"Error in getting memory for matrix");
+        
         } else {
             
-            [self scaleMatrix];
+            [self drawMatrix];
         }
     }
     
@@ -104,130 +94,75 @@
     
     CGPoint start,end;
     int  i = 0;
-        
+    
     start.x = 9999; // x1
     start.y = 9999; // y1
-        
+    
     end.x = 0;      // x2
     end.y = 0;      // y2
-        
+    
+    [self addCustomPoints];
+    
     for (i = 0; i < [pointsGesture count]; i++)
     {
         // Get current point of the drawing
         CGPoint currentPoint = [[pointsGesture objectAtIndex:i]CGPointValue];
-            
+        
         // Determine x1,y1 and x2,y2
         if (currentPoint.x < start.x)   start.x = currentPoint.x;
-            
-        if (currentPoint.x > end.x)     end.x = currentPoint.x;
-            
-        if (currentPoint.y < start.y)   start.y = currentPoint.y;
-            
-        if (currentPoint.y > end.y)     end.y = currentPoint.y;
-            
-    }
         
+        if (currentPoint.x > end.x)     end.x = currentPoint.x;
+        
+        if (currentPoint.y < start.y)   start.y = currentPoint.y;
+        
+        if (currentPoint.y > end.y)     end.y = currentPoint.y;
+        
+    }
+    
     rectDraw = CGRectMake(start.x, start.y, (end.x - start.x), (end.y - start.y));
     
-    NSLog(@"Rect : %f %f %f %f",rectDraw.origin.x,rectDraw.origin.y,(rectDraw.origin.x+rectDraw.size.width),rectDraw.origin.y+rectDraw.size.height);
+    NSLog(@"Rect : %f,%f %f-%f",rectDraw.origin.x,rectDraw.origin.y,(rectDraw.origin.x+rectDraw.size.width),rectDraw.origin.y+rectDraw.size.height);
     
 }
 
-- (void) getBoundsFromPointsProva {
+- (void) addCustomPoints {
     
-    CGPoint start,end;
-    int  i = 0;
+    [pointsGesture removeAllObjects];
     
-    start.x = 9999; // x1
-    start.y = 9999; // y1
-    
-    end.x = 0;      // x2
-    end.y = 0;      // y2
-    
-    prova = [[NSMutableArray alloc]init];
     CGPoint pointTest;
-    pointTest.x = 5;
+    pointTest.x = 1;
     pointTest.y = 3;
     
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
+    [pointsGesture addObject:[NSValue valueWithCGPoint:pointTest]];
     
-    pointTest.x = 5;
+    pointTest.x = 0;
     pointTest.y = 4;
     
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
+    [pointsGesture addObject:[NSValue valueWithCGPoint:pointTest]];
     
-    pointTest.x = 6;
+    pointTest.x = 1;
     pointTest.y = 4;
     
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
+    [pointsGesture addObject:[NSValue valueWithCGPoint:pointTest]];
     
-    pointTest.x = 7;
+    pointTest.x = 2;
     pointTest.y = 4;
     
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
+    [pointsGesture addObject:[NSValue valueWithCGPoint:pointTest]];
     
-    pointTest.x = 8;
-    pointTest.y = 4;
-    
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
-    
-    pointTest.x = 8;
+    pointTest.x = 1;
     pointTest.y = 5;
     
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
-    
-    pointTest.x = 8;
-    pointTest.y = 6;
-    
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
-    
-    pointTest.x = 8;
-    pointTest.y = 7;
-    
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
-    
-    pointTest.x = 7;
-    pointTest.y = 7;
-    
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
-    
-    pointTest.x = 6;
-    pointTest.y = 7;
-    
-    [prova addObject:[NSValue valueWithCGPoint:pointTest]];
-    
-    
-    for (i = 0; i < [prova count]; i++)
-    {
-        // Get current point of the drawing
-        CGPoint currentPoint = [[prova objectAtIndex:i]CGPointValue];
-        
-        NSLog(@"Prova %d : %f %f",i,currentPoint.x,currentPoint.y);
-        
-        // Determine x1,y1 and x2,y2
-        if (currentPoint.x < start.x)   start.x = currentPoint.x;
-        
-        if (currentPoint.x > end.x)     end.x = currentPoint.x;
-        
-        if (currentPoint.y < start.y)   start.y = currentPoint.y;
-        
-        if (currentPoint.y > end.y)     end.y = currentPoint.y;
-        
-    }
-    
-    rectDraw = CGRectMake(start.x, start.y, (end.x - start.x), (end.y - start.y));
-    
-    NSLog(@"Rect : %f %f %f %f",rectDraw.origin.x,rectDraw.origin.y,(rectDraw.origin.x+rectDraw.size.width),rectDraw.origin.y+rectDraw.size.height);
+    [pointsGesture addObject:[NSValue valueWithCGPoint:pointTest]];
     
 }
 
 - (void) assignPoints {
     
-    for(int i = 0 ; i < [prova count]; i++) {
+    for (int i = 0 ; i < [pointsGesture count]; i++) {
         
-        newPointsGesture[i] = [[prova objectAtIndex:i]CGPointValue];
+        newPointsGesture[i] = [[pointsGesture objectAtIndex:i]CGPointValue];
         
-        NSLog(@"New points gesture %i : %f %f",i,newPointsGesture[i].x,newPointsGesture[i].y);
     }
     
     NSLog(@"Points changed");
@@ -238,7 +173,7 @@
     
     NSLog(@"Changing points...");
     
-    for (int i = 0; i < [prova count]; i++) {
+    for (int i = 0; i < [pointsGesture count]; i++) {
         
         newPointsGesture[i].x = newPointsGesture[i].x - rectDraw.origin.x;
         newPointsGesture[i].y = newPointsGesture[i].y - rectDraw.origin.y;
@@ -250,8 +185,11 @@
     
     int error = 0;
     
+    int lines = ((int)rectDraw.size.height+1);
+    int columns = (((int)rectDraw.size.width+1));
+    
     // Define matrixDraw from rectDraw (lines*columns)
-    matrixDraw = (int *)malloc(((int)rectDraw.size.height+1) * (((int)rectDraw.size.width+1) * sizeof(int)));
+    matrixDraw = (int *)malloc(lines * columns * sizeof(int));
     
     if (matrixDraw == NULL) {
         
@@ -259,37 +197,35 @@
         
     } else {
         
-        NSLog(@"Matrix created : fila %d   columna %d ",(int)rectDraw.size.height,(int)rectDraw.size.width);
+        NSLog(@"Matrix [%d][%d]",lines,columns);
         
-        int i = 0;
-        int j = 0;
-        for (int i = 0; i < ((int)rectDraw.size.height+1); i++)
+        NSLog(@"-----------------------------------------------");
+   
+        int i = 0,j = 0;
+        
+        for (i = 0; i < lines; i++)
         {
-            for (int j = 0; j < ((int)rectDraw.size.width+1); j++)
+            for (j = 0; j < columns; j++)
             {
                 if ([self isPoint:i andJ:j] == 1) {
                     
-                    matrixDraw[i*(int)rectDraw.size.height + j] = 1;
+                    matrixDraw[i*lines + j] = 1;
                     
                 } else {
-                    matrixDraw[i*(int)rectDraw.size.height + j] = 0;
+                    
+                    matrixDraw[i*lines + j] = 0;
 
                 }
+                printf("%d",matrixDraw[i*lines + j]);
+
             }
-        }
-        
-        // Assign points to matrix
-        for (i = 0; i < ((int)rectDraw.size.height+1); i++)
-        {
-            for (j = 0; j < ((int)rectDraw.size.width+1); j++)
-            {
-                printf("%d",matrixDraw[i*(int)rectDraw.size.height + j]);
-            }
+            
             printf("\n");
         }
         
-        
-        
+        NSLog(@"-----------------------------------------------");
+
+
         
     }
     
@@ -297,13 +233,11 @@
 
 }
 
-- (int) isPoint:(int)pointI andJ:(int)pointJ {
+- (int)  isPoint:(int)pointI andJ:(int)pointJ {
     
     int ok = 0;
     
-    for (int u = 0; u < [prova count] && ok == 0; u++) {
-        
-        NSLog(@"Comparing %d-%d / %d-%d", (int)newPointsGesture[u].x,pointI,(int)newPointsGesture[u].y,pointJ);
+    for (int u = 0; u < [pointsGesture count] && ok == 0; u++) {
         
         if ((int)newPointsGesture[u].y == pointI && (int)newPointsGesture[u].x == pointJ) {
             
@@ -314,11 +248,51 @@
             ok = 0;
         }
     }
+    
     return ok;
 }
-- (void) scaleMatrix {
+
+#pragma mark - Drawing methods
+- (void) drawMatrix {
     
-    NSLog(@"Scaling matrix...");
+    NSLog(@"Drawing matrix...");
+    
+    int lines = ((int)rectDraw.size.height+1);
+    int columns = (((int)rectDraw.size.width+1));
+    
+    CGSize size = CGSizeMake(lines, columns);
+    UIGraphicsBeginImageContextWithOptions(size, YES, 0);
+    
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            
+            if ( matrixDraw[i*lines + j] == 1 ) {
+                
+                [[UIColor blackColor] setFill];
+                UIRectFill(CGRectMake(i, j, 1, 1));
+                
+            } else {
+                [[UIColor whiteColor] setFill];
+                UIRectFill(CGRectMake(i, j, 1, 1));
+                
+            }
+            
+        }
+    }
+    
+    UIImage *imageFinal = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    [self saveImage:imageFinal];
+
 }
 
+- (void) saveImage: (UIImage *)myImage{
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSData *myImageData = UIImageJPEGRepresentation(myImage, 1.0);
+    [fileManager createFileAtPath:@"/Users/Marc/Desktop/tryImage.jpg" contents:myImageData attributes:nil];
+}
 @end
